7aa5a1591cf6b183e8030d4b7abc9f718075fa74
unknown <williamsartijose@hotmail.com>
2023-03-15 23:38:10 +0000

    12. Tipos de referência de variáveis

--- Day 1/src/Main.java
+++ Day 1/src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         byte minhaIdade = 28;
-        int salarioMensal = 50_000;
-        long idaddeMilisegunddos = 78884000L;
-        double altura = 1.90;
-        char initial = 'A';
-        boolean eMaiorDeIdade = true;
+        String fristname = "William " + "Sarti";
+
         System.out.println(minhaIdade);
-        System.out.println(salarioMensal);
-        System.out.println(idaddeMilisegunddos);
-        System.out.println(altura);
-        System.out.println(initial);
-        System.out.println(eMaiorDeIdade);
+        System.out.println(fristname.toUpperCase());
     }
+
 }
