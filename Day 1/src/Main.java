959643d82479e24b417d106ba8d5a6214599de61
unknown <williamsartijose@hotmail.com>
2023-03-15 23:35:58 +0000

    11. Tipos primitivos de variáveis

--- Day 1/src/Main.java
+++ Day 1/src/Main.java
@@ -1,8 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
-
+        byte minhaIdade = 28;
+        int salarioMensal = 50_000;
+        long idaddeMilisegunddos = 78884000L;
+        double altura = 1.90;
+        char initial = 'A';
+        boolean eMaiorDeIdade = true;
+        System.out.println(minhaIdade);
+        System.out.println(salarioMensal);
+        System.out.println(idaddeMilisegunddos);
+        System.out.println(altura);
+        System.out.println(initial);
+        System.out.println(eMaiorDeIdade);
     }
 }
