8de9fa78252e57fb5d9e242ccce63563caa7cc1e
unknown <williamsartijose@hotmail.com>
2023-03-15 23:50:19 +0000

    17. Dia 1 - Desafio

--- Day 1/src/Main.java
+++ Day 1/src/Main.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //RESOLVER O PROBLEMA DA CALCULADORA DO TIPO 3.0 DOUBLE
+
         //first number:
         Scanner numeroOneScanner =  new Scanner(System.in);
         System.out.print("Enter the first number:");
-        int numeroOne = numeroOneScanner.nextInt();
+        double numeroOne = numeroOneScanner.nextDouble();
 
         //second number:
         Scanner numeroTwoScanner = new Scanner(System.in) ;
         System.out.print("Enter the second number:");
-        int numeroTwo = numeroTwoScanner.nextInt();
+        double numeroTwo = numeroTwoScanner.nextDouble();
 
         //Result
-        int resul = numeroOne + numeroTwo;
-        System.out.println("The result is\n " + resul);
+        double resul = numeroOne + numeroTwo;
+        System.out.println("The result is " + resul);
     }
 
 }
