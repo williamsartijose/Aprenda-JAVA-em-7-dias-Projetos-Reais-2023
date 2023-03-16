ef57eb7189993345cdafb5906fad9b7e31b9db48
unknown <williamsartijose@hotmail.com>
2023-03-16 22:10:44 +0000

    23. Aplicando constantes

--- Day 2/src/Main.java
+++ Day 2/src/Main.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] cars = {{"BMW", "TESLA"}, {"JEEP", "FIAT"}};
-
-        System.out.println(Arrays.deepToString(cars));
+        final double taxaJuros = 11.92; //colocando o FINAL agora ela se torna uma constante
+        System.out.println(taxaJuros);
+      //  taxaJuros = 11.52;
+        System.out.println(taxaJuros);
 
     }
 }
