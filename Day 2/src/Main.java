d8603d19685df59a92f338a94e75461e282f2ba3
unknown <williamsartijose@hotmail.com>
2023-03-16 22:23:12 +0000

    28. Dia 2 - Projeto

--- Day 2/src/Main.java
+++ Day 2/src/Main.java
@@ -3,10 +3,27 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        final double taxaJuros = 11.92; //colocando o FINAL agora ela se torna uma constante
-        System.out.println(taxaJuros);
-      //  taxaJuros = 11.52;
-        System.out.println(taxaJuros);
+        int[] my_array1 = {
 
+                1789, 2035, 1899, 1456, 2013,
+                1458, 2458, 1254, 1472, 2365,
+                1456, 2165, 1457, 2456};
+
+        System.out.println("Array one  - Original: " + Arrays.toString(my_array1));// Ordenado
+        Arrays.sort(my_array1);
+        System.out.println("Array one  - Order: " + Arrays.toString(my_array1));// Original
+
+        String[] my_array2 = {
+                "Java",
+                "Python",
+                "PHP",
+                "C#",
+                "C Programming",
+                "C++"
+        };
+
+        System.out.println("Array two  - Original: " + Arrays.toString(my_array2));// Ordenado
+        Arrays.sort(my_array2);
+        System.out.println("Array two  - Order: " + Arrays.toString(my_array2));// Original
     }
 }
