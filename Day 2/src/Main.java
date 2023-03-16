6505cdf6c1fea49e8f51c7238b5d77ca5046940d
unknown <williamsartijose@hotmail.com>
2023-03-16 22:06:06 +0000

    22. Arrays Multidimensionais

--- Day 2/src/Main.java
+++ Day 2/src/Main.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String[] cars = {"BMW", "TESLA", "JEEP", "FIAT"};
-        cars [2] =  "FERRARI"; //trocar a informação da minha array
-        System.out.println(Arrays.toString(cars));
-        System.out.println(cars[2]); // escolher qual carro da lista
+        String[][] cars = {{"BMW", "TESLA"}, {"JEEP", "FIAT"}};
+
+        System.out.println(Arrays.deepToString(cars));
+
     }
 }
