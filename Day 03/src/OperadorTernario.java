834412912038de7d9865a37b061dfeeb3a304dcb
unknown <williamsartijose@hotmail.com>
2023-03-17 22:06:43 +0000

    37. Operador ternário

---
+++ Day 03/src/OperadorTernario.java
@@ -0,0 +1,14 @@
+public class OperadorTernario {
+    //valor do ingresso vip ou pista
+    public static void  main(String[] args){
+        int ingresso = 800;
+       String terAcesso = ingresso >=500 ? "VIP"  : "PISTA"; //operador ternario ? : 
+
+
+       //codigo grande com if e else
+       // String terAcesso;
+        //if (ingresso >= 500)
+          //  terAcesso = "VIP";
+        //else terAcesso = "PISTA";
+    }
+}
