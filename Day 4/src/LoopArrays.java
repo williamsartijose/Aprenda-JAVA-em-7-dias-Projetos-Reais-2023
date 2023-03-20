ab055441e8fb63b83f713d6e32672162dbbeeebf
unknown <williamsartijose@hotmail.com>
2023-03-20 22:26:36 +0000

    For loops Arrays

---
+++ Day 4/src/LoopArrays.java
@@ -0,0 +1,9 @@
+public class LoopArrays {
+    public static void main(String[] args){
+        String[] cars = {"BMW","Tesla","Fiat"};
+
+        for(int i = 0; i < cars.length; i++)
+            System.out.print(cars[i] + " ");
+
+    }
+}
