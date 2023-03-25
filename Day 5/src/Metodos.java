9b8204e9761511cd1014860d67352c66ec3dbf1f
unknown <williamsartijose@hotmail.com>
2023-03-25 22:58:28 +0000

    Day 5 :  Métodos

---
+++ Day 5/src/Metodos.java
@@ -0,0 +1,12 @@
+public class Metodos {
+    public static void main(String[] args) {
+       productList("Laranja", 12.50);
+       productList("Banana",  13.50);
+    }
+
+    public static void productList(String productName, Double productValue){
+        System.out.println(productName + "- " + productValue);
+
+
+    }
+}
