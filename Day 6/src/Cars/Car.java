679f19e87ff78dd27607831491ef0201750637a5
unknown <williamsartijose@hotmail.com>
2023-03-26 20:51:00 +0100

    Criando um getter

--- Day 6/src/Cars/Car.java
+++ Day 6/src/Cars/Car.java
@@ -10,4 +10,16 @@ public class Car {
         this.modelo = modelo;
         this.ano = ano;
     }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
 }
--- Day 6/src/Cars/Main.java
+++ Day 6/src/Cars/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car("BMW", "X6" , 2024);
-        System.out.println(car.marca); // marca has private access in Cars.Car N acessa por os parametro ser privado
+        System.out.println(car.getMarca()); // marca has private access in Cars.Car N acessa por os parametro ser privado
+        System.out.println(car.getModelo());
+        System.out.println(car.getAno());
     }
 }
