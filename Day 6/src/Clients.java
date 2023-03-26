d4442226d4a7ca7ecbfe625e2bf07e4d394d930e
unknown <williamsartijose@hotmail.com>
2023-03-26 20:28:59 +0100

    Criando o Constructor

--- Day 6/src/Clients.java
+++ Day 6/src/Clients.java
@@ -1,10 +1,13 @@
 public class Clients {
      String firstName;
      int age;
-
+     int balance;
+      Clients(){
+         balance = 10;
+     }
      void newAccount(){
          System.out.println(firstName + "Account Created!");
-         System.out.println("The new balance is $10");
+         System.out.println("The new balance is $" + balance);
      }
 
     }
