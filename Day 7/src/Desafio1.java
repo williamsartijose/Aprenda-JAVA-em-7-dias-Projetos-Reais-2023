bfedcd1b7b7d91798f8080f6c59e4007d0548e3f
unknown <williamsartijose@hotmail.com>
2023-03-27 19:35:36 +0100

    Day 7 - Desafio 1

---
+++ Day 7/src/Desafio1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Desafio1 {
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner (System.in);
+        System.out.println("Digite 3 numeros em seguida  precione ENTER ");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
